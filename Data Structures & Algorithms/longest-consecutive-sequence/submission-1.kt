1efe7bd8d3fadca7667f46e45050b54391815870
Jatin Rastogi <108335884+Jatin7726@users.noreply.github.com>
2026-07-18 22:59:01 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        val nums1 = nums.toSet().sorted().toIntArray()
+        var maxicount = 1
+        var count = 1
+        if(nums.isEmpty()){
+            return 1
+        }
+        for(i in 1 until nums1.size){
+            if(nums1[i] == nums1[i - 1] + 1){
+                count++
+                maxicount = maxOf(maxicount,count)
+            }
+            else{
+                count = 1
+            }
+            
+        }
+        return maxicount
+    }
+}
