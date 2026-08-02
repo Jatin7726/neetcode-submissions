54df073b175ea828869325b9daa0dacc0d2911ec
Jatin Rastogi <108335884+Jatin7726@users.noreply.github.com>
2026-08-02 16:38:27 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        var left =0
+        var right  = nums.size -1
+        while(left<=right){
+            if(nums[left] == nums[right] && abs(left-right) == k){
+                return true
+            }
+            left++
+            right--
+        }
+        return false
+
+    }
+}
