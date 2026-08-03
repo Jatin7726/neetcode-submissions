c460ced382db9b0488443dac96cd6b9a09d5b0ad
Jatin Rastogi <108335884+Jatin7726@users.noreply.github.com>
2026-08-03 22:08:53 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.kt
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        val visited = mutableSetOf<ListNode>()
+        var curr = head
+        while(curr != null){
+            if(visited.contains(curr)){
+                return true
+            }
+            visited.add(curr)
+            curr= curr.next
+        }
+        return false
+
+    }
+}
