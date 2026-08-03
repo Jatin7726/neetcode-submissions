d9244bf9360b945b3d4f3014cb118363454d27a2
Jatin Rastogi <108335884+Jatin7726@users.noreply.github.com>
2026-08-04 00:37:28 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val st = Stack<Char>()
+        for(ch in s){
+            if(ch == '(' || ch == '[' || ch == '{' ){
+                st.push(ch)
+            }
+            else{
+                if(st.isEmpty()){
+                    return false
+                }
+                val top = st.pop()
+
+                if((ch == '}' && top == '{') ||
+                (ch == ')' && top == '(') ||
+                (ch == ']' && top == '[')    ){
+                    st.pop()
+                }
+                else{
+                    return false
+                }
+                
+            }
+        }
+        return st.isEmpty()
+
+    }
+}
