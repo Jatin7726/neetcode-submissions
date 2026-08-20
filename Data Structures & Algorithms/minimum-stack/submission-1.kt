86ad78a385931b4ee5dbf9aa874b54177b0a88ce
Jatin Rastogi <108335884+Jatin7726@users.noreply.github.com>
2026-08-20 22:11:05 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.kt
@@ -0,0 +1,26 @@
+class MinStack() {
+    val stack = ArrayDeque<Int>()
+    val minStack = ArrayDeque<Int>()
+
+    fun push(`val`: Int) {
+        stack.addLast(`val`)
+         if (minStack.isEmpty()) {
+            minStack.addLast(`val`)
+        } else {
+            minStack.addLast(minOf(`val`, minStack.last()))
+        }
+    }
+
+    fun pop() {
+        stack.removeLast()
+        minStack.removeLast()
+    }
+
+    fun top(): Int {
+        return stack.last()
+    }
+
+    fun getMin(): Int {
+        return minStack.last()
+    }
+}
